d6fab42b3b1450407d25022c3f092bc7d254c6f5
Daniel Reynaud <reynauddaniel26@gmail.com>
2019-12-27 15:11:15 +0100

    Reboot spotify instead of muting sound
    Muting sound was good but I had to wait for my music, rebooting spotify kills ads faster

--- spoblock.py
+++ spoblock.py
@@ -7,39 +7,40 @@ import os
 import sys
 import psutil
 import logging
+import subprocess
+from pynput.keyboard import Key, Controller, KeyCode
 
 gi.require_version("Wnck", "3.0")
 from gi.repository import Wnck
 
+keyboard = Controller()
 adsnames = ['Advertisement','Spotify']
-m = alsaaudio.Mixer()
 
-def search(m):
-	addetected = False
+def search():
+	pubdetected = False
 	scr = Wnck.Screen.get_default()
 	scr.force_update()
 	windows = scr.get_windows()
 	for window in windows:
 		if window.get_icon_name() in adsnames:
-			print(window.get_icon_name() + " => Ad detected, muted")
-			addetected = True
-	if addetected:
-		m.setvolume(0)
-	else:
-		m.setvolume(80)
+			print(window.get_icon_name() + " ad detected, rebooting spotify")
+			p = psutil.Process(window.get_pid())
+			p.kill()
+			subprocess.Popen(["nohup", "spotify", ">", "/dev/null", "2>&1&"])
+			time.sleep(4)
+			"""Press play/pause button to launch music """
+			print("play music")
+			keyboard.press(KeyCode.from_vk(269025044))
+			keyboard.release(KeyCode.from_vk(269025044))
 	time.sleep(1)
 
 def restart_program():
 	"""Restarts the current program, with file objects and descriptors cleanup"""
-	try:
-		p = psutil.Process(os.getpid())
-		for handler in p.open_files() + p.connections():
-			os.close(handler.fd)
-	except Exception as e:
-		logging.error(e)
-
+	p = psutil.Process(os.getpid())
+	for handler in p.open_files() + p.connections():
+		os.close(handler.fd)
 	python = sys.executable
 	os.execl(python, python, *sys.argv)
 
-search(m)
+search()
 restart_program()
