215f625dd3eea7e078a32b3bcb1784fbfe094be3
Daniel Reynaud <reynauddaniel26@gmail.com>
2019-12-27 15:39:35 +0100

    Format code
    Clean code using YAPF

--- spoblock.py
+++ spoblock.py
@@ -15,43 +15,51 @@ gi.require_version("Wnck", "3.0")
 from gi.repository import Wnck
 
 keyboard = Controller()
-adsnames = ['Advertisement','Spotify']
+adsnames = ['Advertisement', 'Spotify']
+
 
 def search():
-	scr = Wnck.Screen.get_default()
-	scr.force_update()
-	windows = scr.get_windows()
+    scr = Wnck.Screen.get_default()
+    scr.force_update()
+    windows = scr.get_windows()
+    for window in windows:
+        if window.get_icon_name() in adsnames:
+            """if ads is recognised, reboot spotify """
+            print(
+                window.get_icon_name() + " ad detected -> close spotify ",
+                end='')
+            p = psutil.Process(window.get_pid())
+            p.kill()
+            print(colored('OK', 'green'))
 
-	for window in windows:
-		if window.get_icon_name() in adsnames:
+            run_spotify()
+            time.sleep(5)
 
-			"""if ads is recognised, reboot spotify """
-			print(window.get_icon_name() + " ad detected -> close spotify ", end='')
-			p = psutil.Process(window.get_pid())
-			p.kill()
-			print(colored('OK', 'green'))
+            """Press play/pause button to launch music """
+            print("play music")
+            keyboard.press(KeyCode.from_vk(269025044))
+            keyboard.release(KeyCode.from_vk(269025044))
+    time.sleep(1)
 
-			run_spotify()
-			time.sleep(5)
-
-			"""Press play/pause button to launch music """
-			print("play music")
-			keyboard.press(KeyCode.from_vk(269025044))
-			keyboard.release(KeyCode.from_vk(269025044))
-	time.sleep(1)
 
 def restart_program():
-	"""Restarts the current program, with file objects and descriptors cleanup"""
-	p = psutil.Process(os.getpid())
-	for handler in p.open_files() + p.connections():
-		os.close(handler.fd)
-	python = sys.executable
-	os.execl(python, python, *sys.argv)
+    """Restarts the current program, with file objects and descriptors cleanup"""
+    p = psutil.Process(os.getpid())
+    for handler in p.open_files() + p.connections():
+        os.close(handler.fd)
+    python = sys.executable
+    os.execl(python, python, *sys.argv)
+
 
 def run_spotify():
-	print("Run spotify ", end='')
-	subprocess.Popen(["nohup", "spotify", "&>", "/dev/null", "2>&1&"], shell=False, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
-	print(colored('OK', 'green'))
+    print("Run spotify ", end='')
+    subprocess.Popen(
+        ["nohup", "spotify", "&>", "/dev/null", "2>&1&"],
+        shell=False,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.DEVNULL)
+    print(colored('OK', 'green'))
+
 
 search()
 restart_program()
